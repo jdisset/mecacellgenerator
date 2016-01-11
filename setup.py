e19b73b357f747137d08a232be93053e8c61c22f
jean <j.disset@gmail.com>
2016-01-11 15:53:37 +0100

    correct packaging

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     author_email="j.disset@gmail.com",
     packages=['mecacellgenerator', 'mecacellgenerator.templates'],
     entry_points={
-        'console_scripts': ['mecacellgen = mecacellgenerator:main', ],
-    }
+        'console_scripts': ['mecacellgen = mecacellgenerator.__main__:main', ],
+    },
     include_package_data=True,
     package_data={'mecacellgenerator.templates': ['*']},
     description="Basic project scaffolding tool for the cell simulation library Mecacell",
